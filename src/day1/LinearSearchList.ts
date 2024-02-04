176a2e302ecb97a82f3fae1a7bba4704c6470768
stagefright5 <sankarshanaj@gmail.com>
2024-02-04 15:36:18 +0530

    fix: linearsearch

--- src/day1/LinearSearchList.ts
+++ src/day1/LinearSearchList.ts
@@ -1,3 +1,11 @@
-export default function linear_search(haystack: number[], needle: number): boolean {
-
+export default function linear_search(
+    haystack: number[],
+    needle: number,
+): boolean {
+    for (const h of haystack) {
+        if (h === needle) {
+            return true;
+        }
+    }
+    return false;
 }
